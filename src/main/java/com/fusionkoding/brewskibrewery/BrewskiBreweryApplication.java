76c0ca81245f4a7de79f0f825227022c66f646bb
Kyle Walter <walter.kl26@gmail.com>
2020-10-23 22:14:33 -0500

    Initial commit

---
+++ src/main/java/com/fusionkoding/brewskibrewery/BrewskiBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.fusionkoding.brewskibrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewskiBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewskiBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fusionkoding/brewskibrewery/BrewskiBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusionkoding.brewskibrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewskiBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
